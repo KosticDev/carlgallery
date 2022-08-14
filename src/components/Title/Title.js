351c199ecc4472c29e4ee08401504ade34aab5d8
bluemarkerson <bluemarkerson@gmail.com>
2022-08-15 07:02:47 +0900

    title fix000

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -5,17 +5,24 @@ import img3 from './3.png';
 import img4 from './4.png';
 import img5 from './5.png';
 import video from './1.mp4';
-import logo from './logo.png';
 
 function Title() {
     return (
         <div className='background'>
             <div>
-                <video loop muted autoPlay preload='auto' className='video'>
+                <video loop muted  preload='auto' className='video'>
                     <source src={video} type='video/webm'></source>
                 </video>
             </div>
             <div className='container'>
+                <div className='title'>
+                    <h3>Where code, art and <br />reativity intersect</h3>
+                    <p>A fully on-chain generative art platform that is operated as a <br />DAO. As a member you'll get access to monthly drops from <br />incredible artists.</p>
+                    <div className='title_buttons'>
+                        <button className='coming'>Coming Soon</button>
+                        <button className='learn'>Learn More</button>
+                    </div>
+                </div>
                 <div className='card_lists'>
                     <div className='card'>
                         <img src={img1} alt='' />
