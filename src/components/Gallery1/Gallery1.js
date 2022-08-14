f8c26bdefe627e10d1486452a089e59053bed23b
bluemarkerson <bluemarkerson@gmail.com>
2022-08-15 03:16:34 +0900

    three

--- src/components/Gallery1/Gallery1.js
+++ src/components/Gallery1/Gallery1.js
@@ -1,9 +1,8 @@
-
 function Gallery1() {
     return (
         <div className='container'>
             <div className="gallery1">
-                <h1>Gallery1</h1>>
+                <h1>Gallery1</h1>
             </div>
         </div>
     )
