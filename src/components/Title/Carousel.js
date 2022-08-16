e6be97c1f8edf240bfaac5d46308ba6efe1f69f1
bluemarkerson <bluemarkerson@gmail.com>
2022-08-17 00:15:17 +0900

    jsldkjfi

--- src/components/Title/Carousel.js
+++ src/components/Title/Carousel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import img1 from './1.png';
-import img2 from './2.png';
-import img3 from './3.png';
-import img4 from './4.png';
-import img5 from './5.png';
+import './gallery1.css';
+import card1_1 from '../Gallery1/001/001_1.mp4';
+import card2_1 from '../Gallery1/001/002_1.mp4';
+import card3_1 from '../Gallery1/001/003_1.mp4';
+import card4_1 from '../Gallery1/001/004_1.mp4';
 
 const responsive = {
     0: { items: 1 },
@@ -14,11 +14,34 @@ const responsive = {
 };
 
 const items = [
-    <div className="item" data-value="1"><img className='carousel' src={img1} /></div>,
-    <div className="item" data-value="2"><img className='carousel' src={img2} /></div>,
-    <div className="item" data-value="3"><img className='carousel' src={img3} /></div>,
-    <div className="item" data-value="4"><img className='carousel' src={img4} /></div>,
-    <div className="item" data-value="5"><img className='carousel' src={img5} /></div>,
+    <div className="item" data-value="1">
+        <div>
+            <video loop muted autoPlay preload='auto' className='video'>
+                <source src={card1_1} type='video/webm'></source>
+            </video>
+        </div>
+    </div>,
+    <div className="item" data-value="2">
+        <div>
+            <video loop muted autoPlay preload='auto' className='video'>
+                <source src={card2_1} type='video/webm'></source>
+            </video>
+        </div>
+    </div>,
+    <div className="item" data-value="3">
+        <div>
+            <video loop muted autoPlay preload='auto' className='video'>
+                <source src={card3_1} type='video/webm'></source>
+            </video>
+        </div>
+    </div>,
+    <div className="item" data-value="4">
+        <div>
+            <video loop muted autoPlay preload='auto' className='video'>
+                <source src={card4_1} type='video/webm'></source>
+            </video>
+        </div>
+    </div>,
 ];
 
 const Carousel = () => (
